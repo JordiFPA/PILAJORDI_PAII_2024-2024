3230ce8c913c0cbea97daf351aed229cb16c3363
Jordi Pila <jfpila@uce.edu.ec>
2024-04-22 15:22:03 -0500

    Mi primer proyecto.

---
+++ Proyecto_001/src/proyecto1/App.java
@@ -0,0 +1,12 @@
+package proyecto1;
+
+import javax.swing.JFrame;
+
+public class App {
+
+	public static void main(String[] args) {
+		new OpenGLFrame( "JOGL demo", 1024, 600);
+		
+	}
+
+}
---
+++ Proyecto_001/src/proyecto1/OpenGLFrame.java
@@ -0,0 +1,134 @@
+package proyecto1;
+
+import java.awt.GridLayout;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import com.jogamp.opengl.GL;
+import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLEventListener;
+import com.jogamp.opengl.awt.GLJPanel;
+
+public class OpenGLFrame extends JFrame implements GLEventListener {
+
+	private static final long serialVersionUID =1L; 
+	private GLJPanel glJPanel;
+	private String typeGraph = "Circle"; 
+	
+	
+	public OpenGLFrame(String title, int width, int height) {
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle(title);
+		this.setSize(width, height);
+		this.setLocation(270, 65);
+		this.setLayout(new GridLayout(0,2));
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		
+		JButton btn1 = new JButton("Triángulo");
+		btn1.addActionListener(new ActionListener() {			
+		
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				drawing("Triangle");
+				
+			}
+		});
+		panel.add(btn1);
+		
+		JButton btn2 = new JButton("Cuadrado");
+		btn2.addActionListener(new ActionListener() {			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				drawing("Square");
+			}
+		});
+		panel.add(btn2);
+		
+		JButton btn3 = new JButton("Círculo");
+		btn3.addActionListener(new ActionListener() {			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				drawing("Circle");
+			}
+		});
+		panel.add(btn3);
+		
+		this.add(panel);
+
+		glJPanel = new GLJPanel();
+		glJPanel.addGLEventListener(this);		
+		this.add(glJPanel);
+		this.setVisible(true);
+		
+	}
+	
+	public void drawing(String type) {
+		typeGraph = type;
+		display(glJPanel);
+	}
+	
+	
+	@Override
+	public void display(GLAutoDrawable drawable) {
+		GL2 gl = drawable.getGL().getGL2();
+		gl.glClear(GL2.GL_COLOR_BUFFER_BIT);
+		if(typeGraph.equals("Triangle")) {
+			gl.glBegin(GL.GL_TRIANGLES);
+			gl.glColor3f(1.0f, 0.8f, 0.5f);
+			gl.glVertex3f(-0.5f, -0.5f, 0.0f);
+			gl.glVertex3f(0.0f, 0.5f, 0.0f);
+			gl.glVertex3f(0.5f, -0.5f, 0.0f);
+			gl.glEnd();
+		}else {
+			if(typeGraph.equals("Square")) {
+				gl.glBegin(GL2.GL_QUADS);
+				gl.glVertex3f( 0.5f,0.5f,0);
+				gl.glVertex3f(-0.5f,0.5f,0);
+				gl.glVertex3f(-0.5f,-0.5f,0);
+				gl.glVertex3f(0.5f,-0.5f,0);
+				gl.glEnd();
+			}else {
+				gl.glBegin(GL2.GL_POLYGON);
+				int numVertices = 100;
+		        double radius = 0.5;
+				double angle = 0;
+	            double angleIncrement = 2 * Math.PI / numVertices;
+	            for (int i = 0; i < numVertices; i++) {
+	                angle = i * angleIncrement;
+	                double x = radius * Math.cos(angle);
+	                double y = radius * Math.sin(angle);
+	                gl.glVertex2d(x, y);
+	            }
+				gl.glEnd();
+			}
+		}
+		glJPanel.repaint();
+		
+	}
+
+	@Override
+	public void dispose(GLAutoDrawable drawable) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(GLAutoDrawable drawable) {	
+	}
+
+	@Override
+	public void reshape(GLAutoDrawable arg0, int arg1, int arg2, int arg3, int arg4) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
